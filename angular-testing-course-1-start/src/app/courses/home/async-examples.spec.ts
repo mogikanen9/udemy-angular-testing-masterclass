6d72eb3e25b65d0bfa54504f7dc684cf10465de6
Vlad Voinov <mogikanen9@users.noreply.github.com>
2021-09-30 17:44:21 -0400

    Update async-examples.spec.ts

--- angular-testing-course-1-start/src/app/courses/home/async-examples.spec.ts
+++ angular-testing-course-1-start/src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,5 @@
+import { fakeAsync, flush, tick } from "@angular/core/testing";
+
 fdescribe('Async Test Examples',()=>{
 
     it('async test example with jasmin done()',(done:DoneFn)=>{
@@ -12,6 +14,43 @@ fdescribe('Async Test Examples',()=>{
 
          },1000);
 
+    });
+
+    it('async test example - setTimeout', fakeAsync(()=>{
+        let flag = false;
+
+        setTimeout(()=>{},100);
+
+         setTimeout(()=>{
+            flag = true;
+         },1000);
+
+         // tick(1000);
+
+         flush(); // execute all timeouts
+
+         expect(flag).toBeTruthy();
+
+    }));
+
+    fit('Async test example - plain Promise', ()=>{
+        let flag = false;
+        console.log('Creating promise');
+
+        setTimeout(()=>{
+            console.log('set timeout');
+        });
+
+        queueMicrotask(()=>{
+            console.log('my microtask');
+        });
+
+        Promise.resolve().then(()=>{
+            console.log('Promise evaluated successfully');
+            flag = true;
+        })
+        console.log('Runnig test assertions');
+        expect(flag).toBeTruthy();
     });
 
 });
